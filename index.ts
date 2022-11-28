20f41e23307bd76acd53221264066ad7801fdb92
Valeria Proshachenko <vproshachenko@gmail.com>
2022-11-28 13:43:42 +0200

    init

---
+++ index.ts
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const nodeFetch = require('node-fetch');
+const { Telegraf } = require('telegraf')
+
+
+const users = [
+    {userName: 'quartz555', firstName: 'Bogdan', birthday: '05.05.2007'},
+    {userName: 'Miksam_13', firstName: 'Bogdan', birthday: '13.12.2008'},
+    {userName: 'vad22', firstName: 'Bogdan',  birthday: '05.05.2007'},
+    {userName: 'vl_pnk', firstName: 'Bogdan', birthday: '11.01.2008'},
+    {userName: 'ju_dio', firstName: 'Bogdan', birthday: '03.06.2009'}
+]
+
+async function getUsesrData(): Promise<void>{
+    const resp = await nodeFetch(process.env.API_KEY)
+    const data = await resp.json()
+    return data
+}
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+
+bot.telegram.setMyCommands([
+    {command: "/start", description: 'start bot'},
+    {command: "/help", description: 'help command'},
+    {command: '/about', description: 'about bot'},
+    {command: '/team', description: 'about bot team'}
+]);
+bot.start(async ctx => ctx.reply(process.env.START_MESSAGE));
+bot.command('about', async ctx => await ctx.reply(process.env.ABOUT_MESSAGE));
+bot.command('team', async ctx => await ctx.reply(process.env.TEAM_MESSAGE));
+bot.help(async ctx => await ctx.reply(process.env.HELP_MESSAGE))
+bot.launch()
