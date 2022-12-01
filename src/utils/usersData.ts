0e6049957055e60f38bb9975bb77ba8ad1a4ac49
Viktoriia Hrechukha <>
2022-12-01 23:29:49 +0100

    [ba] Added solution for getting spreadsheet data via googleapis

--- src/utils/usersData.ts
+++ src/utils/usersData.ts
@@ -8,3 +8,44 @@ async function getUserData(): Promise<any> {
 }
 
 export default getUserData;
+
+// PROPOSED SOLUTION:
+// const {google} = require('googleapis');
+// const sheets = google.sheets('v4');
+//
+// type RowItemType = [
+// 	'Отметка времени',
+// 	'Адрес электронной почты',
+// 	'Ник гитхаба',
+// 	'лет',
+// 	'Мотивация того, почему ты тут',
+// 	'Дата рождения (для вкусняшек)',
+// 	'Хочу делать',
+// 	'Сколько времени в неделю в среднем уделяешь программированию(в часах)?',
+// 	'Чего хотелось бы больше?',
+// 	'Ник в телеграме'
+// ];
+//
+// const zipArrays = (keysArray: string[], valuesArray: string[]) =>
+// 	Object.fromEntries(
+// 		keysArray.map((value, index) => [value, valuesArray[index]]),
+// 	);
+//
+// async function getUserData(): Promise<any> {
+// 	try {
+// 		const response = (await sheets.spreadsheets.values.get({
+// 			spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+// 			range: ['A1:J14'],
+// 			auth: process.env.GOOGLE_SPREADSHEET_API_KEY,
+// 		})).data;
+//
+// 		const rowNames = response.values[0];
+// 		const rowItems = response.values.slice(1);
+//
+// 		return rowItems.map((rowItem: RowItemType) => zipArrays(rowNames, rowItem));
+// 	} catch (err) {
+// 		console.error(err);
+// 	}
+// }
+//
+// export default getUserData;
