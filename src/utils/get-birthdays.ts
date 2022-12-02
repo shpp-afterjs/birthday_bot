d565a263071a1660cbf0827f5d606da22b4fd49f
Valeria Proshachenko <vproshachenko@gmail.com>
2022-12-02 17:21:33 +0200

    rewrite get-birthdays func with reduce

--- src/utils/get-birthdays.ts
+++ src/utils/get-birthdays.ts
@@ -2,24 +2,23 @@ import { User } from '../interfaces/user.interface';
 
 import getUserData from './usersData';
 
-async function getBirthdays(): Promise<{ birthdaysLeft: string; birthdaysWillBe: string; }> {
+async function getBirthdays(): Promise<any> {
 	const users:User[] = await getUserData();
 	const now = new Date();
 	const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-	let birthdaysWillBe: string = '';
-	let birthdaysLeft: string = '';
-
-	users.forEach(item => {
+	const object = users.reduce((obj: {birthdaysWillBe: string, birthdaysLeft: string }, item) => {
 		const arr = item.birthday.split('.');
 		const userDay = new Date(`${now.getFullYear()},${arr[1]},${arr[0]}`);
 		if (userDay.getTime() - today.getTime() < 0) {
-			birthdaysLeft += `${item.username}\n`;
+			obj.birthdaysLeft += `${item.username}\n`;
 		} else if (userDay.getTime() - today.getTime() > 0) {
-			birthdaysWillBe += `${item.username}\n`;
+			obj.birthdaysWillBe += `${item.username}\n`;
 		}
-	});
 
-	return {birthdaysLeft, birthdaysWillBe};
+		return obj;
+	}, {birthdaysWillBe: '', birthdaysLeft: '' });
+
+	return object;
 }
 
 export default getBirthdays;
