3d5e2df1f16260295adecdba0f9d023818713c06
Valeria Proshachenko <vproshachenko@gmail.com>
2022-12-11 15:01:16 +0200

    add descriptions

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -2,9 +2,9 @@ const messages = {
 	HELP_MESSAGE:
 	`/help - list of commands.\r
 	/about - about this bot.\r
-	/getBirthday {@nickname} - description\r
-	/getPastBirthdays  - description\r
-	/getFutureBirthdays - description\r
+	/getBirthday {@nickname} - return days left until birthday \r
+	/getPastBirthdays  - return list of members who already had birthday this year\r
+	/getFutureBirthdays - return list of members who will have birthday this year \r
 	/birthdaysList  - list of birthday dates\r
 	/getAge {number} - return age of member\r
 	/whoHasThisAge {@nickname} - return people who have this age`,
